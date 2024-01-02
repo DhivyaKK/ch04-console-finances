e9701f1a05d501669aa1db5cdad2fe984949e204
Dhivya <dhivyakotha@gmail.com>
2024-01-02 22:13:06 +0000

    formatted output, js code and added comments

--- starter/index.js
+++ starter/index.js
@@ -1,142 +1,163 @@
 //Given the financial dataset
 var finances = [
-  ['Jan-2010', 867884],
-  ['Feb-2010', 984655],
-  ['Mar-2010', 322013],
-  ['Apr-2010', -69417],
-  ['May-2010', 310503],
-  ['Jun-2010', 522857],
-  ['Jul-2010', 1033096],
-  ['Aug-2010', 604885],
-  ['Sep-2010', -216386],
-  ['Oct-2010', 477532],
-  ['Nov-2010', 893810],
-  ['Dec-2010', -80353],
-  ['Jan-2011', 779806],
-  ['Feb-2011', -335203],
-  ['Mar-2011', 697845],
-  ['Apr-2011', 793163],
-  ['May-2011', 485070],
-  ['Jun-2011', 584122],
-  ['Jul-2011', 62729],
-  ['Aug-2011', 668179],
-  ['Sep-2011', 899906],
-  ['Oct-2011', 834719],
-  ['Nov-2011', 132003],
-  ['Dec-2011', 309978],
-  ['Jan-2012', -755566],
-  ['Feb-2012', 1170593],
-  ['Mar-2012', 252788],
-  ['Apr-2012', 1151518],
-  ['May-2012', 817256],
-  ['Jun-2012', 570757],
-  ['Jul-2012', 506702],
-  ['Aug-2012', -1022534],
-  ['Sep-2012', 475062],
-  ['Oct-2012', 779976],
-  ['Nov-2012', 144175],
-  ['Dec-2012', 542494],
-  ['Jan-2013', 359333],
-  ['Feb-2013', 321469],
-  ['Mar-2013', 67780],
-  ['Apr-2013', 471435],
-  ['May-2013', 565603],
-  ['Jun-2013', 872480],
-  ['Jul-2013', 789480],
-  ['Aug-2013', 999942],
-  ['Sep-2013', -1196225],
-  ['Oct-2013', 268997],
-  ['Nov-2013', -687986],
-  ['Dec-2013', 1150461],
-  ['Jan-2014', 682458],
-  ['Feb-2014', 617856],
-  ['Mar-2014', 824098],
-  ['Apr-2014', 581943],
-  ['May-2014', 132864],
-  ['Jun-2014', 448062],
-  ['Jul-2014', 689161],
-  ['Aug-2014', 800701],
-  ['Sep-2014', 1166643],
-  ['Oct-2014', 947333],
-  ['Nov-2014', 578668],
-  ['Dec-2014', 988505],
-  ['Jan-2015', 1139715],
-  ['Feb-2015', 1029471],
-  ['Mar-2015', 687533],
-  ['Apr-2015', -524626],
-  ['May-2015', 158620],
-  ['Jun-2015', 87795],
-  ['Jul-2015', 423389],
-  ['Aug-2015', 840723],
-  ['Sep-2015', 568529],
-  ['Oct-2015', 332067],
-  ['Nov-2015', 989499],
-  ['Dec-2015', 778237],
-  ['Jan-2016', 650000],
-  ['Feb-2016', -1100387],
-  ['Mar-2016', -174946],
-  ['Apr-2016', 757143],
-  ['May-2016', 445709],
-  ['Jun-2016', 712961],
-  ['Jul-2016', -1163797],
-  ['Aug-2016', 569899],
-  ['Sep-2016', 768450],
-  ['Oct-2016', 102685],
-  ['Nov-2016', 795914],
-  ['Dec-2016', 60988],
-  ['Jan-2017', 138230],
-  ['Feb-2017', 671099],
+  ["Jan-2010", 867884],
+  ["Feb-2010", 984655],
+  ["Mar-2010", 322013],
+  ["Apr-2010", -69417],
+  ["May-2010", 310503],
+  ["Jun-2010", 522857],
+  ["Jul-2010", 1033096],
+  ["Aug-2010", 604885],
+  ["Sep-2010", -216386],
+  ["Oct-2010", 477532],
+  ["Nov-2010", 893810],
+  ["Dec-2010", -80353],
+  ["Jan-2011", 779806],
+  ["Feb-2011", -335203],
+  ["Mar-2011", 697845],
+  ["Apr-2011", 793163],
+  ["May-2011", 485070],
+  ["Jun-2011", 584122],
+  ["Jul-2011", 62729],
+  ["Aug-2011", 668179],
+  ["Sep-2011", 899906],
+  ["Oct-2011", 834719],
+  ["Nov-2011", 132003],
+  ["Dec-2011", 309978],
+  ["Jan-2012", -755566],
+  ["Feb-2012", 1170593],
+  ["Mar-2012", 252788],
+  ["Apr-2012", 1151518],
+  ["May-2012", 817256],
+  ["Jun-2012", 570757],
+  ["Jul-2012", 506702],
+  ["Aug-2012", -1022534],
+  ["Sep-2012", 475062],
+  ["Oct-2012", 779976],
+  ["Nov-2012", 144175],
+  ["Dec-2012", 542494],
+  ["Jan-2013", 359333],
+  ["Feb-2013", 321469],
+  ["Mar-2013", 67780],
+  ["Apr-2013", 471435],
+  ["May-2013", 565603],
+  ["Jun-2013", 872480],
+  ["Jul-2013", 789480],
+  ["Aug-2013", 999942],
+  ["Sep-2013", -1196225],
+  ["Oct-2013", 268997],
+  ["Nov-2013", -687986],
+  ["Dec-2013", 1150461],
+  ["Jan-2014", 682458],
+  ["Feb-2014", 617856],
+  ["Mar-2014", 824098],
+  ["Apr-2014", 581943],
+  ["May-2014", 132864],
+  ["Jun-2014", 448062],
+  ["Jul-2014", 689161],
+  ["Aug-2014", 800701],
+  ["Sep-2014", 1166643],
+  ["Oct-2014", 947333],
+  ["Nov-2014", 578668],
+  ["Dec-2014", 988505],
+  ["Jan-2015", 1139715],
+  ["Feb-2015", 1029471],
+  ["Mar-2015", 687533],
+  ["Apr-2015", -524626],
+  ["May-2015", 158620],
+  ["Jun-2015", 87795],
+  ["Jul-2015", 423389],
+  ["Aug-2015", 840723],
+  ["Sep-2015", 568529],
+  ["Oct-2015", 332067],
+  ["Nov-2015", 989499],
+  ["Dec-2015", 778237],
+  ["Jan-2016", 650000],
+  ["Feb-2016", -1100387],
+  ["Mar-2016", -174946],
+  ["Apr-2016", 757143],
+  ["May-2016", 445709],
+  ["Jun-2016", 712961],
+  ["Jul-2016", -1163797],
+  ["Aug-2016", 569899],
+  ["Sep-2016", 768450],
+  ["Oct-2016", 102685],
+  ["Nov-2016", 795914],
+  ["Dec-2016", 60988],
+  ["Jan-2017", 138230],
+  ["Feb-2017", 671099],
 ];
 
 //variable declaration
 var totalNoOfMonths = 0;
 var totalProfit = 0;
-var avgChange = 0;
-var maxIncreasePF =0;
-var maxDecreasePF = 0;
-var temp = [];
-var maxChange =[];
+var sumOfChangeInProfit = 0;
+var maxIncreasePL = 0;
+var maxDecreasePL = 0;
+var tempCalc = [];
+var changeInProfitPerMonth = [];
+
+console.log("Given financial dataset is :" + "\n" + finances);
 
 //The total number of months included in the dataset
 totalNoOfMonths = finances.length;
 
-console.log("Total months : " +  totalNoOfMonths);
+console.log("Total months : " + totalNoOfMonths);
 
 //The net total amount of Profit/Losses over the entire period
 
-for(var i=0; i<finances.length; i++)
- {
-   totalProfit = totalProfit + finances[i][1];
-  
- }
-
-console.log("Total profit: " + "$"+ totalProfit);
-
-for(var i=0; i<finances.length-1; i++)
- {
- temp[i,i] =[finances[i+1][0], finances[i+1][1]-finances[i][1]];
-
- //to calculate sum of change in profit each month
- avgChange += finances[i+1][1]-finances[i][1];
-
- maxChange[i] = (finances[i+1][1]-finances[i][1]);
- 
+for (var i = 0; i < finances.length; i++) {
+  totalProfit = totalProfit + finances[i][1];
 }
 
+console.log("Total profit: " + "$" + totalProfit);
 
-//Average change in profit for the given data
-console.log("Avgerage change : " +  (avgChange) /85);
+//length-1 to avoid index overflow for the last item in array
+for (var i = 0; i < finances.length - 1; i++) {
+  //get the month and change in profit/loss per month
+  tempCalc[(i, i)] = [finances[i + 1][0], finances[i + 1][1] - finances[i][1]];
 
-maxIncreasePF = Math.max(...maxChange);
-maxDecreasePF = Math.min(...maxChange);
-console.log ("Greatest Increase in Profits/Losses:" + temp[maxChange.indexOf(maxIncreasePF)]);
-console.log ("Greatest Decrease in Profits/Losses:" + temp[maxChange.indexOf(maxDecreasePF)]);
+  //to calculate sum of change in profit each month changeInProfitPerMonth
+  sumOfChangeInProfit += finances[i + 1][1] - finances[i][1];
 
+  //store change in profit/loss per month in an array
+  changeInProfitPerMonth[i] = finances[i + 1][1] - finances[i][1];
+}
 
+//Average change in profit for the given data rounding to 2 decimal values
+console.log(
+  "Avgerage change : " +
+    Math.round((sumOfChangeInProfit / (finances.length - 1)) * 100) / 100
+);
 
+//... is called spread over operator to expand an iterable inside specified receiver
+//to get max of the values in the array
+maxIncreasePL = Math.max(...changeInProfitPerMonth);
 
+//to get min of the values in the array
+maxDecreasePL = Math.min(...changeInProfitPerMonth);
 
+for (let i = 0; i < tempCalc.length; i++) {
+  if (tempCalc[(i, i)][1] === maxIncreasePL) {
+    console.log(
+      "Greatest Increase in Profits/Losses: " +
+        tempCalc[(i, i)][0] +
+        " ($" +
+        maxIncreasePL +
+        ")"
+    );
+  }
+  if (tempCalc[(i, i)][1] === maxDecreasePL) {
+    console.log(
+      "Greatest Decrease in Profits/Losses: " +
+        tempCalc[(i, i)][0] +
+        " ($" +
+        maxDecreasePL +
+        ")"
+    );
+  }
+}
 
-
-
-
+//can also be done this way
+// console.log ("Greatest Increase in Profits/Losses:" + temp[changeInProfitPerMonth.indexOf(maxIncreasePL)]);
+// console.log ("Greatest Decrease in Profits/Losses:" + temp[changeInProfitPerMonth.indexOf(maxDecreasePL)]);
